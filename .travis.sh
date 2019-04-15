39e9be568a2ad6b81ecec3ae9362d924dcaf204c
ytake <yuuki.takezawa@comnect.jp.net>
2019-04-15 11:06:56 +0900

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -2,6 +2,12 @@
 set -ex
 apt update -y
 DEBIAN_FRONTEND=noninteractive apt install -y memcached redis-server php-cli zip unzip
+
+systemctl status memcached
+systemctl start memcached
+systemctl status redis-server
+systemctl start redis-server
+
 hhvm --version
 php --version
 
